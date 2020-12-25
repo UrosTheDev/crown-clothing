dabe0f6533ea00e2eaf5769e7bef7bc5f5864141
Uros <uros.tomasevic@jsguru.io>
2020-12-25 20:30:01 +0100

    added header component and links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/header';
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 
@@ -8,6 +9,7 @@ import './App.scss';
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={Homepage} />
                 <Route path='/shop' component={ShopPage} />
--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './collection-item.scss';
 
-
-
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-item'>
         <div
